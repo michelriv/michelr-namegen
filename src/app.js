5075d996b6b8585d97ce1335ebef9cdeec70af1e
michelriv <157402536+michelriv@users.noreply.github.com>
2024-02-22 01:33:52 +0000

    project

--- src/app.js
+++ src/app.js
@@ -8,4 +8,19 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  // create arrays for domain name generation
+  let pronoun = ["the", "our", "my"];
+  let adj = ["great", "big", "amazing"];
+  let noun = ["jogger", "racoon", "dog", "student"];
+
+  console.log(pronoun[0] + adj[0] + noun[0] + ".com");
+  console.log(pronoun[0] + adj[0] + noun[1] + ".com");
+  console.log(pronoun[0] + adj[0] + noun[2] + ".com");
+
+  for (let i = 0; i < pronoun.length; ++i) {
+    for (let j = 0; j < adj.length; ++j) {
+      for (let k = 0; k < noun.length; ++k) {
+        console.log(pronoun [i] + adj [j] + noun [k] + ".com")
+    }
+  }
 };
