9362484fa54ada58d164f078e74f7dd37a5e890c
michelriv <157402536+michelriv@users.noreply.github.com>
2024-02-24 01:49:30 +0000

    closing bracket added

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ window.onload = function() {
     for (let j = 0; j < adj.length; ++j) {
       for (let k = 0; k < noun.length; ++k) {
         console.log(pronoun [i] + adj [j] + noun [k] + ".com")
+      } 
     }
   }
-}
 };
